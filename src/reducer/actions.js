e6c6eb69b751f8c7ca583a8d391aa07dec583ddf
rowaxl <rowaxl0@gmail.com>
2020-12-16 08:17:57 +0900

    connect dispatch and selector

--- src/reducer/actions.js
+++ src/reducer/actions.js
@@ -1,13 +1,13 @@
 import * as ActionTypes from './types'
 
-export const addNewRow = (newRow) => (dispatch) => {
-  return dispatch({ type: ActionTypes.ADD_ROW, payload: newRow })
+export const addRow = (newRow) => {
+  return { type: ActionTypes.ADD_ROW, payload: newRow }
 }
 
-export const editRow = (updatedRow) => (dispatch) => {
-  return dispatch({ type: ActionTypes.EDIT_ROW, payload: updatedRow })
+export const editRow = (updatedRow) => {
+  return { type: ActionTypes.EDIT_ROW, payload: updatedRow }
 }
 
-export const deleteRow = (targetRowId) => (dispatch) => {
-  return dispatch({ type: ActionTypes.DELETE_ROW, payload: targetRowId })
+export const deleteRow = (targetRowId) => {
+  return { type: ActionTypes.DELETE_ROW, payload: targetRowId }
 }
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,8 +2,13 @@ import * as ActionTypes from './types'
 
 const initialState = {
   dataRows: [
-    // TODO: Delete dummy row
-    { id: '762821087', title: 'PR #38750 - fix(docs-infra): typography, layout and UI polish', state: 'open', url: 'https://api.github.com/repos/angular/angular/issues/40093', createdAt: '2020-12-11T19:50:02Z', updatedAt: '2020-12-11T19:59:42Z' }
+    { id: '762821087', title: 'PR #38750 - fix(docs-infra): typography, layout and UI polish', state: 'open', url: 'https://api.github.com/repos/angular/angular/issues/40093', created: '2020-12-11T19:50:02Z', updated: '2020-12-11T19:59:42Z' },
+    { id: '762821089', title: 'PR #38750 - fix(docs-infra): typography, layout and UI polish', state: 'open', url: 'https://api.github.com/repos/angular/angular/issues/40093', created: '2020-12-11T19:50:02Z', updated: '2020-12-11T19:59:42Z' },
+    { id: '762821090', title: 'PR #38750 - fix(docs-infra): typography, layout and UI polish', state: 'open', url: 'https://api.github.com/repos/angular/angular/issues/40093', created: '2020-12-11T19:50:02Z', updated: '2020-12-11T19:59:42Z' },
+    { id: '762821091', title: 'PR #38750 - fix(docs-infra): typography, layout and UI polish', state: 'open', url: 'https://api.github.com/repos/angular/angular/issues/40093', created: '2020-12-11T19:50:02Z', updated: '2020-12-11T19:59:42Z' },
+    { id: '762821092', title: 'PR #38750 - fix(docs-infra): typography, layout and UI polish', state: 'open', url: 'https://api.github.com/repos/angular/angular/issues/40093', created: '2020-12-11T19:50:02Z', updated: '2020-12-11T19:59:42Z' },
+    { id: '762821093', title: 'PR #38750 - fix(docs-infra): typography, layout and UI polish', state: 'open', url: 'https://api.github.com/repos/angular/angular/issues/40093', created: '2020-12-11T19:50:02Z', updated: '2020-12-11T19:59:42Z' },
+    { id: '762821094', title: 'PR #38750 - fix(docs-infra): typography, layout and UI polish', state: 'open', url: 'https://api.github.com/repos/angular/angular/issues/40093', created: '2020-12-11T19:50:02Z', updated: '2020-12-11T19:59:42Z' },
   ]
 }
 
